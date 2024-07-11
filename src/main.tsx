c54446602b06e0ea20d6792946b0fc6223303a63
Park YuongHo <hexdrinker2020@gmail.com>
2024-07-11 19:16:58 +0900

    라우팅 적용

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,37 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import JotaiPage from './pages/JotaiPage.tsx'
+import ReduxPage from './pages/ReduxPage.tsx'
+import MobxPage from './pages/MobxPage.tsx'
+import RecoilPage from './pages/RecoilPage.tsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+  },
+  {
+    path: '/jotai',
+    element: <JotaiPage />,
+  },
+  {
+    path: '/redux',
+    element: <ReduxPage />,
+  },
+  {
+    path: '/mobx',
+    element: <MobxPage />,
+  },
+  {
+    path: '/recoil',
+    element: <RecoilPage />,
+  },
+])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <RouterProvider router={router} />
+  </React.StrictMode>
 )

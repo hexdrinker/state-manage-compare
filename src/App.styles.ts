6111721477bfca56005ec39a82ff11f31025f682
Park YuongHo <hexdrinker2020@gmail.com>
2024-07-11 19:16:21 +0900

    글로벌 스타일 수정

---
+++ src/App.styles.ts
@@ -0,0 +1,20 @@
+import styled from '@emotion/styled'
+
+const Container = styled.main`
+  width: 100vw;
+  height: 100vh;
+`
+
+const ListWrapper = styled.div`
+  max-width: 1200px;
+  height: 900px;
+  margin: 0 auto;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
+
+export default {
+  Container,
+  ListWrapper,
+}
